ddac6abfbb9275ac95ffa7747c2debfcf61a7319
Paliao <andre_paliao@hotmail.com>
2018-03-01 16:35:19 -0300

    Firebase and store created

--- App.js
+++ App.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import firebase from 'firebase'
 
-export default class App extends React.Component {
-  render() {
+import reducers from './src/reducers'
+
+const store = createStore(reducers)
+
+export default class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+        apiKey: "AIzaSyCnVsqjhRjHZdEE5EzdZhAY0pqmqYX9C54",
+        authDomain: "maneger-2b9ee.firebaseapp.com",
+        databaseURL: "https://maneger-2b9ee.firebaseio.com",
+        projectId: "maneger-2b9ee",
+        storageBucket: "maneger-2b9ee.appspot.com",
+        messagingSenderId: "943233819444"
+    })
+  }
+
+  render(){
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+      <Provider store={store}>
+        <View style={{flex: 1}}>
+          <Text>Hello!</Text>
+        </View>
+      </Provider>
+    )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
